57e4ffd14132bc9f6e4d3dd7de063c34785ac694
gabrielaauzcategui@gmail.com <gabrielaauzcategui@gmail.com>
2022-02-15 16:34:44 +0100

    avance cliente

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,21 +2,17 @@ import { Badge } from "@material-ui/core";
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
-import { mobile } from "../responsive";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import logo from '../assets/img/logo.svg'
+
 
 const Container = styled.div`
   height: 60px;
-  ${mobile({ height: "50px" })}
 `;
-
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  ${mobile({ padding: "10px 0px" })}
 `;
 
 const Left = styled.div`
@@ -28,11 +24,9 @@ const Left = styled.div`
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
-  ${mobile({ display: "none" })}
 `;
-
 const SearchContainer = styled.div`
-  border: 0.5px solid lightgray;
+  border: 0.5px solid lightgrey;
   display: flex;
   align-items: center;
   margin-left: 25px;
@@ -41,7 +35,6 @@ const SearchContainer = styled.div`
 
 const Input = styled.input`
   border: none;
-  ${mobile({ width: "50px" })}
 `;
 
 const Center = styled.div`
@@ -50,49 +43,44 @@ const Center = styled.div`
 `;
 
 const Logo = styled.h1`
-  font-weight: bold;
-  ${mobile({ fontSize: "24px" })}
+  font-weigth: bold;
 `;
+
 const Right = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: flex-end;
-  ${mobile({ flex: 2, justifyContent: "center" })}
 `;
 
 const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
   margin-left: 25px;
-  ${mobile({ fontSize: "12px", marginLeft: "10px" })}
 `;
 
 const Navbar = () => {
-  const quantity = useSelector(state=>state.cart.quantity)
   return (
     <Container>
       <Wrapper>
         <Left>
-          <Language>EN</Language>
+          <Language>ES</Language>
           <SearchContainer>
-            <Input placeholder="Search" />
-            <Search style={{ color: "gray", fontSize: 16 }} />
+            <Input></Input>
+            <Search style={{color:"grey", fontSize:16}}></Search>
           </SearchContainer>
         </Left>
         <Center>
-          <Logo>LAMA.</Logo>
+          <img src={logo}></img>
         </Center>
         <Right>
           <MenuItem>REGISTER</MenuItem>
           <MenuItem>SIGN IN</MenuItem>
-          <Link to="/cart">
           <MenuItem>
-            <Badge badgeContent={quantity} color="primary">
+            <Badge badgeContent={4} color="primary">
               <ShoppingCartOutlined />
             </Badge>
           </MenuItem>
-          </Link>
         </Right>
       </Wrapper>
     </Container>
@@ -100,3 +88,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -1,8 +1,9 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
 import { useState } from "react";
 import styled from "styled-components";
+import foto1 from '../assets/img/foto1.jpg'
 import { sliderItems } from "../data";
-import { mobile } from "../responsive";
+import { mobile } from "../responsive"; 
 
 const Container = styled.div`
   width: 100%;
@@ -16,7 +17,7 @@ const Container = styled.div`
 const Arrow = styled.div`
   width: 50px;
   height: 50px;
-  background-color: #fff7f7;
+  background-color: #eaeaea;
   border-radius: 50%;
   display: flex;
   align-items: center;
@@ -44,16 +45,14 @@ const Slide = styled.div`
   height: 100vh;
   display: flex;
   align-items: center;
-  background-color: #${(props) => props.bg};
+  background-color:${props=>props.bg};
 `;
-
 const ImgContainer = styled.div`
   height: 100%;
   flex: 1;
 `;
-
 const Image = styled.img`
-  height: 80%;
+  height: 70%;
 `;
 
 const InfoContainer = styled.div`
@@ -62,53 +61,52 @@ const InfoContainer = styled.div`
 `;
 
 const Title = styled.h1`
-  font-size: 70px;
+    font-size: 70px;
 `;
 
 const Desc = styled.p`
-  margin: 50px 0px;
-  font-size: 20px;
-  font-weight: 500;
-  letter-spacing: 3px;
+    margin: 50px 0px;
+    font-size: 20px;
+    font-weight: 400;
+    letter-spacing: 3px;
 `;
-
 const Button = styled.button`
-  padding: 10px;
-  font-size: 20px;
-  background-color: transparent;
-  cursor: pointer;
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
 `;
 
 const Slider = () => {
-  const [slideIndex, setSlideIndex] = useState(0);
-  const handleClick = (direction) => {
-    if (direction === "left") {
-      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
-    } else {
-      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
-    }
-  };
+    const [slideIndex, setSlideIndex] = useState(0);
+    const handleClick = (direction)=>{
+        if (direction === "left") {
+            setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+          } else {
+            setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+          }
+    };
 
   return (
     <Container>
-      <Arrow direction="left" onClick={() => handleClick("left")}>
+      <Arrow direction="left" onClick={()=>handleClick("left")}>
         <ArrowLeftOutlined />
       </Arrow>
       <Wrapper slideIndex={slideIndex}>
-        {sliderItems.map((item) => (
-          <Slide bg={item.bg} key={item.id}>
-            <ImgContainer>
-              <Image src={item.img} />
-            </ImgContainer>
-            <InfoContainer>
-              <Title>{item.title}</Title>
-              <Desc>{item.desc}</Desc>
-              <Button>SHOW NOW</Button>
-            </InfoContainer>
-          </Slide>
-        ))}
-      </Wrapper>
-      <Arrow direction="right" onClick={() => handleClick("right")}>
+            {sliderItems.map((item) => (
+                <Slide bg={item.bg} key={item.id}>
+                    <ImgContainer>
+                    <Image src={foto1} />
+                    </ImgContainer>
+                    <InfoContainer>
+                    <Title>{item.title}</Title>
+                    <Desc>{item.desc}</Desc>
+                    <Button>Pide tu cita</Button>
+                    </InfoContainer>
+                </Slide>
+                ))}
+        </Wrapper>
+      <Arrow direction="right" onClick={()=>handleClick("right")}>
         <ArrowRightOutlined />
       </Arrow>
     </Container>
